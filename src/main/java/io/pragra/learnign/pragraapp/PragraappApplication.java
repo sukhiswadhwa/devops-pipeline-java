564b298da5eec0763f9df34c241a5fd5bab6450e
Atin S <atin@pragra.io>
2022-09-18 10:00:45 -0400

    Initial Commit

---
+++ src/main/java/io/pragra/learnign/pragraapp/PragraappApplication.java
@@ -0,0 +1,13 @@
+package io.pragra.learnign.pragraapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PragraappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PragraappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/pragra/learnign/pragraapp/PragraappApplicationTests.java
@@ -0,0 +1,13 @@
+package io.pragra.learnign.pragraapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PragraappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
